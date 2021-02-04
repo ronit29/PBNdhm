069cac1d48b9f4cad5610d82abc41c4dc51a4fb0
ETEC\RachitSaxena <rachitsaxena@policybazaar.com>
2021-02-04 10:39:23 +0530

    health profile changes

--- src/main/java/com/pb/dp/dao/HealthQuery.java
+++ src/main/java/com/pb/dp/dao/HealthQuery.java
@@ -6,5 +6,6 @@ public interface HealthQuery {
 	String GET_OTP = "select c.id, c.otp from customer c(nolock) where c.mobile=?";
 	String UPDATE_TXN_ID = "UPDATE DocprimeNDHM.dbo.customer SET txnId=? WHERE id=?";
 	String GET_CUST_HEALTH_PROFILE = "select top 1 c.id as customerId,c.mobile,c.firstName,c.midName,c.lastName,c.dob,c.relationship,c.emailId,c.gender,h.healthId,h.healtIdNo,c.address_id as addressId,h.isKyc from customer c(nolock) inner join healthId h(nolock) on h.customerId=c.id where c.id=? and h.isActive=1 ORDER by c.createdAt DESC";
+	String GET_HEALTH_TOKEN = "select healthIdToken from healthId (nolock) where healthId=?";
 	
 }
--- src/main/java/com/pb/dp/service/HealthServiceImpl.java
+++ src/main/java/com/pb/dp/service/HealthServiceImpl.java
@@ -1,13 +1,18 @@
 package com.pb.dp.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.google.gson.Gson;
 import com.pb.dp.dao.HealthDao;
 import com.pb.dp.model.CustomerHealth;
 import com.pb.dp.model.GetHealthProfileRequest;
+import com.pb.dp.util.AuthTokenUtil;
+import com.pb.dp.util.HttpUtil;
 
 @Service
 public class HealthServiceImpl implements HealthService {
@@ -15,17 +20,67 @@ public class HealthServiceImpl implements HealthService {
 	@Autowired
 	private HealthDao healthDao;
 	
+	@Autowired
+	private ConfigService configService;
+	
+	@Autowired
+	private AuthTokenUtil authTokenUtil;
+	
 	@Override
 	public List<CustomerHealth> getCustHealthDetails(long mobileNo, int customerId) {
 		return healthDao.getCustHealthDetails(mobileNo,customerId);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public CustomerHealth getHealthProfile(int customerId, GetHealthProfileRequest custHealthOtpRequest)
 			throws Exception {
 		CustomerHealth response = healthDao.getHealthProfile(customerId);
 		if(null!=custHealthOtpRequest.getHealthId() && !custHealthOtpRequest.getHealthId().isEmpty()) {
-			//TODO call the Apis 
+			StringBuilder xToken = new StringBuilder("Bearer "); 
+			String authToken = healthDao.getHealthToken(custHealthOtpRequest.getHealthId());
+			if (null != authToken) {
+				Map<String, String> header = new HashMap<>();
+				String token = authTokenUtil.bearerAuthToken();
+				header.put("Authorization", token);
+				header.put("X-HIP-ID", "DPHIP119");
+				Map<String, Object> jsonMap = new HashMap<>();
+				jsonMap.put("authToken", authToken);
+				String jsonPayload = new Gson().toJson(jsonMap);
+				String url = configService.getPropertyConfig("NDHM_ACCOUNT_TOKEN_URL").getValue();
+				Map<String,Object> responseFromApi = HttpUtil.post(url, jsonPayload, header);
+				int statusCode = (int) responseFromApi.get("status");
+				if(statusCode == 200) {
+					String responseBody = (String)responseFromApi.get("responseBody");
+					Boolean isValidBoolean = Boolean.valueOf(responseBody);
+					if(!isValidBoolean) {
+						xToken.append(authToken);
+						Map<String, String> header2 = new HashMap<>();
+						header2.put("Authorization", token);
+						header2.put("X-HIP-ID", "DPHIP119");
+						header2.put("X-Token", xToken.toString());
+						String url2 = configService.getPropertyConfig("NDHM_ACCOUNT_PROFILE_URL").getValue();
+						Map<String,Object> responseFromApi2 = HttpUtil.post(url2, jsonPayload, header2);
+						int statusCode2 = (int) responseFromApi2.get("status");
+						if(statusCode2 == 200) {
+							String responseBody2 = (String)responseFromApi2.get("responseBody");
+							Map<String,Object> responseMap = (Map<String,Object>) new Gson().fromJson(responseBody2, Map.class);
+							if(null!=responseMap) {
+								response.setAddress((String)responseMap.get("address"));
+								response.setState((String)responseMap.get("stateName"));
+								response.setDistrict((String)responseMap.get("districtName"));
+								response.setHealthId((String)responseMap.get("healthId"));
+								response.setHealtIdNo((String)responseMap.get("healthIdNumber"));
+								response.setEmailId((String)responseMap.get("email"));
+								response.setGender((String)responseMap.get("gender"));
+								response.setFirstName((String)responseMap.get("firstName"));
+								response.setMidName((String)responseMap.get("middleName"));
+								response.setLastName((String)responseMap.get("lastName"));
+							}
+						}
+					}
+				}
+			}
 		}
 		return response;
 	}
