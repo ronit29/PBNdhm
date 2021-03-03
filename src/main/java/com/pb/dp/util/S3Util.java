5bf2bc3893b8cdb2d68389a031519c600a57469f
ronit <ronitd@paisabazaar.com>
2021-03-03 22:47:54 +0530

    up

--- src/main/java/com/pb/dp/util/S3Util.java
+++ src/main/java/com/pb/dp/util/S3Util.java
@@ -5,10 +5,7 @@ import org.slf4j.LoggerFactory;
 import com.pb.dp.util.HttpUtil;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.http.HttpResponse;
@@ -34,5 +31,36 @@ public class S3Util{
         File convertedFile = convertMultiPartToFile(file);
         Map<String, String> responseMap  = HttpUtil.postRequestMultiPart(uri, convertedFile, jsonPayload, header);
         return "test";
+    }
+
+    public String localFileUpload(MultipartFile file) {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+
+        String rootPath = System.getProperty("user.dir");
+        File dir = new File(rootPath + File.separator + "webapp"+File.separator+"res"+File.separator+"img");
+        if (!dir.exists())
+            dir.mkdirs();
+        String fileName = file.getOriginalFilename();
+        File newFile = new File(dir.getAbsolutePath() + File.separator + fileName);
+
+        try {
+            inputStream = file.getInputStream();
+
+            if (!newFile.exists()) {
+                newFile.createNewFile();
+            }
+            outputStream = new FileOutputStream(newFile);
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return newFile.getAbsolutePath();
     }
 }
